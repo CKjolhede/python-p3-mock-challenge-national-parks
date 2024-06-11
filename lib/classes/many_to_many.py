260b587b993c6280505c793d15f71d9304de01b5
ckjolhede <ckjolhede@gmail.com>
2024-06-11 15:58:55 -0600

    working

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,40 +1,61 @@
 class NationalPark:
 
     def __init__(self, name):
-        self.name = name
+        self._name = name
         
-    def trips(self):
-        pass
+    @property
+    def name(self):
+        return self._name
     
+    @name.setter
+    def name(self, value):
+        if isinstance (value, str) and len(value) >= 3 and not hasattr(self, "name"):
+            self._name = value
+        else:
+            return
+                
+    def trips(self):
+        return [trip for trip in Trip.all if trip.national_park == self]
+        
     def visitors(self):
-        pass
+        return list({trip.visitor for trip in self.trips()})
     
     def total_visits(self):
-        pass
+        visitors_list = [trip.visitor for trip in self.trips()]
+        return len(visitors_list) if visitors_list else print(int("0"))
     
     def best_visitor(self):
-        pass
-
+        for visitor in self.visitors():
+            num_visitor_trips = {visitor: visitor.total_visits_at_park(self)}
+    
+        sorted_dict = dict(sorted(num_visitor_trips.items(), key=lambda x: max(x[1]), reverse=True))
+        return sorted_dict.keys[0]
 
 class Trip:
+    
+    all = []
     
     def __init__(self, visitor, national_park, start_date, end_date):
         self.visitor = visitor
         self.national_park = national_park
         self.start_date = start_date
         self.end_date = end_date
+        Trip.all.append(self)
 
 
 class Visitor:
+    
+    all = []
 
     def __init__(self, name):
         self.name = name
+        Visitor.all.append(self)
         
     def trips(self):
-        pass
+        return [trip for trip in Trip.all if trip.visitor == self]
     
     def national_parks(self):
-        pass
+        return {trip.national_park for trip in self.trips}
     
     def total_visits_at_park(self, park):
-        pass
+        return len([trip for trip in self.trips() if trip.national_park == park])
--- lib/debug.py
+++ lib/debug.py
@@ -6,6 +6,16 @@ from classes.many_to_many import Visitor
 from classes.many_to_many import Trip
 
 if __name__ == '__main__':
-    print("HELLO! :) let's debug :vibing_potato:")
+    
+        yosemite = NationalPark("Yosemite")
+        rocky = NationalPark("RMNP")
+        lantz = Visitor("Lantz")
+        chris = Visitor("Chris")
+        matt = Visitor("Matt")
+        trip_1 = Trip(matt, yosemite, "May 5th", "May 9th")
+        trip_2 = Trip(matt, yosemite, "May 20th", "May 27th")
+        trip_3 = Trip(chris, yosemite, "May 5th", "May 9th")
+        trip_4 = Trip(lantz, yosemite, "May 5th", "May 9th")
+        trip_5 = Trip(chris, rocky, "May 20th", "June 10th")
 
-    ipdb.set_trace()
+ipdb.set_trace()
